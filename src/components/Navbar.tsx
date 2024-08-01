0a54961e8ec1df2f0b967e75babfde0c408f6995
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-08-01 17:33:50 +0500

    add useRef

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 type NavbarProps = {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
@@ -5,6 +7,25 @@ type NavbarProps = {
 };
 
 function Navbar({ count, query, setQuery }: NavbarProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(
+    function () {
+      const callback = (e: KeyboardEvent) => {
+        if (document.activeElement === inputRef.current) return;
+
+        if (e.code === "Enter") {
+          inputRef.current?.focus();
+          setQuery("");
+        }
+      };
+
+      document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
+
   return (
     <div className="navbar box-shadow">
       <a className="text-secondary flex-1 font-bold text-2xl">
@@ -18,6 +39,7 @@ function Navbar({ count, query, setQuery }: NavbarProps) {
             type="text"
             placeholder="Search"
             className="input input-bordered w-32 md:w-auto"
+            ref={inputRef}
           />
         </div>
         <p className="text-md font-semibold">
